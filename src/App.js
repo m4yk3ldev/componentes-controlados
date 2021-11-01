89cc9b13db0cce3f72d151169100ef8c80cf3e7b
Maykel Arias Torres <hanibal920915@gmail.com>
2021-11-01 11:09:31 -0400

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import {useState} from 'react';
+import {useState} from 'react'
 
-const  App= () => {
+const App = () => {
+    const [form, setForm] = useState({normal: '', text: '', select: '', check: false})
 
-  const [value,setValue] = useState({normal:'por defecto', texto:'', select: '', check: false})
-  const handleChange = ({target}) => {
-    setValue((state) => ({
-      ...state,
-      [target.name]: target.type==='checkbox'? target.checked : target.value
-    }))
-  }
-  console.log(value)
+    //const handleChange = () => {
+    //setForm({ normal: e.target.value })
+    //}
+    const handleChange = ({target}) => {
+        setForm(state => ({
+            ...state,
+            [target.name]: target.type === 'checkbox' ? target.checked : target.value
+        }))
+    }
 
-  return (
-    <div>
-      {value.normal.length < 5 ? <span>longitud minima de 5</span>: null}
-      <input name='normal' value={value.normal} onChange={handleChange}/>  
-      <textarea name='texto' onChange={handleChange} value={value.texto}/>
-      <select value={value.select} name='select' onChange={handleChange}>
-        <option value=''>---Seleccione---</option>
-        <option value='chancofeliz'>Chancho feliz</option>
-        <option value='chanchitofeliz'>Chanchito feliz</option>
-        <option value='chanchitotriste'>Chanchito triste</option>
-      </select>
-      <input
-        type='checkbox'
-        name='check'
-        onChange={handleChange}
-        checked={value.check}
-        value={value.check}
-      />
-    </div> 
-  );
+    const submit = () => {
+        console.log(form)
+    }
+
+    return (
+        <div>
+            <input onChange={handleChange} name='normal' value={form.normal}/>
+
+            <textarea onChange={handleChange} name='text' value={form.text}/>
+
+            <select value={form.select} name='select' onChange={handleChange}>
+                <option value=''>-- Seleccione --</option>
+                <option value='chanchofeliz'>Chancho feliz</option>
+                <option value='chanchitofeliz'>Chanchito feliz</option>
+                <option value='chanchitotriste'>Chanchito triste</option>
+                <option value='felipe'>Felipe</option>
+            </select>
+            <input
+                type="checkbox"
+                onChange={handleChange}
+                name='check'
+                checked={form.check}
+            />
+            <div onChange={handleChange}>
+                <label>Chancho</label>
+                <input type='radio' value='feliz' name='estado'/> Feliz
+                <input type='radio' value='triste' name='estado'/> Triste
+                <input type='radio' value='emocionado' name='estado'/> Emocionado
+            </div>
+            <button onClick={submit}>Enviar</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
