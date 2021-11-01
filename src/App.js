0bae58b8b459610adeedfd3ea94f5f07dccd064c
Maykel Arias Torres <hanibal920915@gmail.com>
2021-11-01 10:39:09 -0400

    Agregado campos de checkbox y select

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+const  App= () => {
+
+  const [value,setValue] = useState({normal:'por defecto', texto:'', select: '', check: false})
+  const handleChange = ({target}) => {
+    setValue((state) => ({
+      ...state,
+      [target.name]: target.type==='checkbox'? target.checked : target.value
+    }))
+  }
+  console.log(value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {value.normal.length < 5 ? <span>longitud minima de 5</span>: null}
+      <input name='normal' value={value.normal} onChange={handleChange}/>  
+      <textarea name='texto' onChange={handleChange} value={value.texto}/>
+      <select value={value.select} name='select' onChange={handleChange}>
+        <option value=''>---Seleccione---</option>
+        <option value='chancofeliz'>Chancho feliz</option>
+        <option value='chanchitofeliz'>Chanchito feliz</option>
+        <option value='chanchitotriste'>Chanchito triste</option>
+      </select>
+      <input
+        type='checkbox'
+        name='check'
+        onChange={handleChange}
+        checked={value.check}
+        value={value.check}
+      />
+    </div> 
   );
 }
 
